cc9ce8e056775b7fd823937ae23fa914c9690b3e
Abhimanyu Kapur <abhi.kapur09@gmail.com>
2019-06-02 14:03:17 -0400

    S3 WORKS

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import * as Posts from './controllers/post_controller';
 import * as UserController from './controllers/user_controller';
 import { requireAuth, requireSignin } from './services/passport';
+import signS3 from './services/s3';
 
 
 const router = Router();
@@ -25,5 +26,7 @@ router.route('/auth')
 
 router.post('/signin', requireSignin, UserController.signin);
 router.post('/signup', UserController.signup);
+
+router.get('/sign-s3', signS3);
 
 export default router;
